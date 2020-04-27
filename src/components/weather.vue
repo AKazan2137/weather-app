3013ad36b7f67fe7dc4b7c73ec95257eaf32e729
ne0nekk <ne0nekk@gmail.com>
2020-04-27 15:42:03 +0200

    add api from openweathermap, add bootstrap

--- src/components/weather.vue
+++ src/components/weather.vue
@@ -1,24 +1,32 @@
 <template>
-  <b-container class="bv-example-row">
+  <b-container class="bv-example-row" v-if="list">
     <b-row>
       <b-col>
-        <b-list-group v-for="weath in weather" :key="weath.id">
+        <b-list-group v-for="weath in list.data.list" :key="weath.dt_txt">
           <b-list-group-item button @click="selectedWeather=weath">
             <a>
-              <span>{{weath.id}}</span>
+              <span>{{weath.dt_txt}}</span>
             </a>
           </b-list-group-item>
         </b-list-group>
       </b-col>
-      <b-col>
+      <b-col v-if="selectedWeather">
         <b-list-group>
-          <b-list-group-item disabled>
-            <a>
-              <input v-model="selectedWeather.id" />
-              <input v-model="selectedWeather.country" />
-              <input v-model="selectedWeather.city" />
-              <input v-model="selectedWeather.tempToday" />
-            </a>
+          <b-list-group-item>
+            <b-row class="my-1">
+              <b-col sm="3">
+                <label>temp:</label>
+                <label>feels_like:</label>
+                <label>temp_min:</label>
+                <label>temp_max:</label>
+              </b-col>
+              <b-col sm="3">
+                <b-form-input disabled v-model="selectedWeather.main.temp">temp</b-form-input>
+                <b-form-input disabled v-model="selectedWeather.main.feels_like" />
+                <b-form-input disabled v-model="selectedWeather.main.temp_min" />
+                <b-form-input disabled v-model="selectedWeather.main.temp_max" />
+              </b-col>
+            </b-row>
           </b-list-group-item>
         </b-list-group>
       </b-col>
@@ -27,6 +35,7 @@
 </template>
 
 <script>
+import * as axios from "axios";
 export default {
   name: "weather",
   props: {
@@ -34,51 +43,20 @@ export default {
   },
   data() {
     return {
-      selectedWeather: {
-        id: 1,
-        country: "",
-        city: "",
-        tempToday: 0
-      },
-      weather: [
-        {
-          id: 1,
-          country: "Poland",
-          city: "Warsaw",
-          tempToday: 20.5
-        },
-        {
-          id: 2,
-          country: "Poland",
-          city: "Cracow",
-          tempToday: 22
-        },
-        {
-          id: 3,
-          country: "Poland",
-          city: "Gdynia",
-          tempToday: 23
-        }
-      ]
+      selectedWeather: undefined,
+      showMore: false,
+      list: []
     };
+  },
+  created() {},
+  methods: {},
+  mounted() {
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/forecast?q=Warsaw&appid=9513741eb68c088a78d7e4fd5622e253&units=metric"
+      )
+      .then(response => (this.list = response));
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
